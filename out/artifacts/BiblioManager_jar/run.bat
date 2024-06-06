0ea2dc5f1de5db2aefb61757113c8795f237a296
Yahir <yar2_22@alu.uabcs.mx>
2024-06-06 05:01:13 -0700

    executable jar added

--- out/artifacts/BiblioManager_jar/run.bat
+++ out/artifacts/BiblioManager_jar/run.bat
@@ -1,3 +1,8 @@
 @echo off
+
+set SQL_URL=jdbc:mysql://monorail.proxy.rlwy.net:54121/railway
+set SQL_USER=root
+set     SQL_PASSWORD=OVlKTqoHQsCrRbSOaJLVFoWiTtwXjkKb
+
 java --add-opens java.base/java.lang.reflect=ALL-UNNAMED -jar BiblioManager.jar
 pause
